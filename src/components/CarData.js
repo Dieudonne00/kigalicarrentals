b4cd1b31fb6a14a391889c2f5c889a477825f424
Dieudonne00 <146090340+Dieudonne00@users.noreply.github.com>
2026-04-05 12:08:36 +0200

    Refactor CAR_DATA to use image URLs and new attributes
    Updated car data structure to include image URLs and additional attributes such as category and updated prices.

--- src/components/CarData.js
+++ src/components/CarData.js
@@ -1,93 +1,10 @@
-import Golf6 from "../images/cars-big/golf6.jpg";
-import AudiA1 from "../images/cars-big/audia1.jpg";
-import Toyota from "../images/cars-big/toyotacamry.jpg";
-import Bmw320 from "../images/cars-big/bmw320.jpg";
-import Benz from "../images/cars-big/benz.jpg";
-import Passat from "../images/cars-big/passatcc.jpg";
+const BASE = "https://carrentalinkigali.com/myimages";
 
 export const CAR_DATA = [
-  [
-    {
-      name: "VW Golf 6",
-      price: "37",
-      img: Golf6,
-      model: "Golf 6",
-      mark: "Volkswagen",
-      year: "2008",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-  ],
-  [
-    {
-      name: "Audi A1 S-Line",
-      price: "45",
-      img: AudiA1,
-      model: "Audi",
-      mark: "A1",
-      year: "2012",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Manual",
-      fuel: "Gasoline",
-    },
-  ],
-  [
-    {
-      name: "Toyota Camry",
-      price: "30",
-      img: Toyota,
-      model: "Camry",
-      mark: "Toyota",
-      year: "2006",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Automatic",
-      fuel: "Hybrid",
-    },
-  ],
-  [
-    {
-      name: "BMW 320 ModernLine",
-      price: "35",
-      img: Bmw320,
-      model: "320",
-      mark: "BMW",
-      year: "2012",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-  ],
-  [
-    {
-      name: "Mercedes-Benz GLK",
-      price: "50",
-      img: Benz,
-      model: "Benz GLK",
-      mark: "Mercedes",
-      year: "2006",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Manual",
-      fuel: "Diesel",
-    },
-  ],
-  [
-    {
-      name: "VW Passat CC",
-      price: "25",
-      img: Passat,
-      model: "Passat CC",
-      mark: "Volkswagen",
-      year: "2008",
-      doors: "4/5",
-      air: "Yes",
-      transmission: "Automatic",
-      fuel: "Gasoline",
-    },
-  ],
+  [{ name: "VW Golf 6", price: "45", img: `${BASE}/cars/golf6.jpg`, model: "Golf 6", mark: "Volkswagen", year: "2008", doors: "4/5", air: "Yes", transmission: "Manual", fuel: "Diesel", category: "Economy" }],
+  [{ name: "Audi A1 S-Line", price: "75", img: `${BASE}/cars/audia1.jpg`, model: "Audi", mark: "A1 S-Line", year: "2012", doors: "4/5", air: "Yes", transmission: "Manual", fuel: "Gasoline", category: "Compact" }],
+  [{ name: "Toyota Camry", price: "60", img: `${BASE}/cars/toyotacamry.jpg`, model: "Camry", mark: "Toyota", year: "2018", doors: "4/5", air: "Yes", transmission: "Automatic", fuel: "Hybrid", category: "Sedan" }],
+  [{ name: "BMW 320 ModernLine", price: "90", img: `${BASE}/cars/bmw320.jpg`, model: "320", mark: "BMW", year: "2015", doors: "4/5", air: "Yes", transmission: "Automatic", fuel: "Diesel", category: "Luxury" }],
+  [{ name: "Mercedes-Benz GLK", price: "110", img: `${BASE}/cars/benz.jpg`, model: "GLK", mark: "Mercedes-Benz", year: "2014", doors: "4/5", air: "Yes", transmission: "Automatic", fuel: "Diesel", category: "SUV" }],
+  [{ name: "VW Passat CC", price: "55", img: `${BASE}/cars/passatcc.jpg`, model: "Passat CC", mark: "Volkswagen", year: "2012", doors: "4/5", air: "Yes", transmission: "Automatic", fuel: "Gasoline", category: "Sedan" }],
 ];
